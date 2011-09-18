e0ad4e7b08253021eca3da1d06489da8e2f49b42
zamaz <zamaz@users.sourceforge.net>
2011-09-18 12:42:20 +0200

    sdl: bring back GL_LINEAR
    Using GL_CLAMP_TO_EDGE instead of GL_CLAMP removes the visible border
    between textures.
    
    Signed-off-by: zamaz <zamaz@users.sourceforge.net>

--- sdl/sdl.cpp
+++ sdl/sdl.cpp
@@ -363,10 +363,10 @@ static void maketex(GLuint *id, void *buffer, int width)
 {
 	glGenTextures(1, id);
 	glBindTexture(GL_TEXTURE_2D, *id);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, 256, 0, GL_RGBA,
 		     GL_UNSIGNED_BYTE, buffer);
 }
